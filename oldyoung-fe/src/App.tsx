d4fbd77391b01b796c68f4fff45be246a917c7ef
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-08-13 17:30:33 +0900

    setting/oldyoung

--- oldyoung-fe/src/App.tsx
+++ oldyoung-fe/src/App.tsx
@@ -1,8 +1,59 @@
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import LoginPage from './pages/Login';
+import SigninPage from './pages/Signin';
+import LoginSuccess from './pages/Auth/LoginSuccess';
+import UserLayout from './layouts/UserLayout';
+import HomePage from './pages/Home';
+
+const publicRoutes = createBrowserRouter([
+  {
+    path: '/',
+    element: <UserLayout />,
+    children: [
+      {
+        index: true,
+        // loader: AuthCheck.authPageCheck,
+        element: <LoginPage />,
+        handle: { isPublic: true },
+      },
+      {
+        path: 'signin',
+        // loader: AuthCheck.authPageCheck,
+        element: <SigninPage />,
+        handle: { isPublic: true },
+      },
+      {
+        path: 'login/success',
+        element: <LoginSuccess />,
+        handle: { isPublic: true },
+      },
+      {
+        path: 'home',
+        element: <HomePage />,
+        handle: { isPublic: true }, 
+      },
+    ],
+  },
+]);
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <div></div>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={publicRoutes} />
+      {import.meta.env.DEV && <ReactQueryDevtools initialIsOpen={false} />}
+    </QueryClientProvider>
+  );
+};
 
 export default App;
--- oldyoung-fe/vite.config.ts
+++ oldyoung-fe/vite.config.ts
@@ -1,11 +1,83 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgr from 'vite-plugin-svgr'
+import { VitePWA } from 'vite-plugin-pwa'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    global: 'globalThis',
+  },
   plugins: [
-    react(),
+    react(), 
     tailwindcss(),
+    svgr(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: "LOOPy",
+        name: "LOOPy | 고객의 루틴에 나의 커피를 더하다",
+        icons: [
+          {
+            src: "/icon.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "/icon2.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/icon3.png",
+            type: "image/png",
+            sizes: "512x512",
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#6970F3",        
+        background_color: "#6970F3"
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024,
+        
+        globPatterns: [
+          'index.html',
+          'manifest.webmanifest',
+          '**/*.{js,css,ico,png,svg,jpg,jpeg,webp}'
+        ],
+        runtimeCaching: [
+          {
+            // API 요청은 항상 네트워크
+            urlPattern: /^https:\/\/api\..*$/i,
+            handler: 'NetworkOnly',
+            options: {
+              cacheName: 'loopy-pwa-cache-v1',
+              expiration: {
+                maxEntries: 100,
+                maxAgeSeconds: 24 * 60 * 60,
+              },
+            },
+          },
+          {
+            // 외부 이미지 요청은 캐시
+            urlPattern: /^https:\/\/.*\.(?:png|jpg|jpeg|webp|gif|svg)$/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'external-images',
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 7 * 24 * 60 * 60,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      }
+    }),
   ],
-})
+});
