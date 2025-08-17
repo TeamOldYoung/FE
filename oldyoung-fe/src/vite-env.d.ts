94a6fbe484ac517a4e90813d7e91e665fb066ea0
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-08-17 20:50:57 +0900

    fix/setting

--- oldyoung-fe/src/vite-env.d.ts
+++ oldyoung-fe/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVER_API_URL: string;
+  readonly VITE_KAKAO_JS_KEY: string;
+  readonly VITE_KAKAO_REST_API_KEY: string;    
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
