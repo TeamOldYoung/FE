6e804eaee22637f6ffd6093e0aac2d2433de759d
고민균 <97932282+skyblue1232@users.noreply.github.com>
2025-08-19 01:19:39 +0900

    style/voicesearch-page

--- oldyoung-fe/src/pages/Voice/index.tsx
+++ oldyoung-fe/src/pages/Voice/index.tsx
@@ -1,4 +1,5 @@
 import CommonHeader from "../../components/header/CommonHeader";
+import WarningIcon from "../../assets/icons/Warning.svg?react";
 
 interface VoicePageProps {
   onBack: () => void;
@@ -6,11 +7,30 @@ interface VoicePageProps {
 
 const VoicePage = ({ onBack }: VoicePageProps) => {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex flex-col relative h-screen">
       <CommonHeader title="음성 검색" onBack={onBack} />
 
-      <div className="mt-[3.25rem] px-[1.25rem]">
-        내용
+      <div className="mt-[3rem] px-[1.5rem] text-start text-[#4F4F4F] text-[1.125rem] font-medium">
+        음성으로 손쉽게 필요한 기능을 찾아보세요 :)
+      </div>
+
+      <div className="fixed inset-0 bg-[#747775]/50 flex justify-center items-center z-50">
+        <div className="mt-20 mx-[1.25rem] bg-white border-[#BBEFE9] rounded-[1.25rem] border-[3px] w-full px-12 py-9 sm:max-w-[393px] flex flex-col items-center">
+          <div className="mb-5.5 mt-2.5">
+            <WarningIcon />
+          </div>
+         
+          <p className="text-center text-[1.25rem] mb-8.5 font-medium">
+            아직 개발 중인 서비스입니다
+          </p>
+
+          <button
+            className="w-full py-2 rounded-full bg-[#2DC5B3] text-[1.25rem] text-white font-semibold leading-[1.5rem]"
+            onClick={onBack}
+          >
+            홈화면으로 돌아가기
+          </button>
+        </div>
       </div>
     </div>
   );
